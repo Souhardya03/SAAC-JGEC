f9d7a2713149184460b33d395c9aeed8d97cd3b1
darkwarrior2003 <souhardyadeb2016@gmail.com>
2024-06-24 13:21:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/Home';
-import Footer from './components/Footer/Footer';
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import { AnimatePresence } from "framer-motion";
+import Banner from "./pages/Banner/Banner";
+import About from "./pages/About/About";
+import Aos from "aos";
 
 function App() {
+  Aos.init({
+    duration: 2000,
+    once: true,
+  });
+  const location = useLocation();
+
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/home' element={<Home/>}/>
-    </Routes>
-    <Footer/>
+      <Navbar />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route exact path="/" element={<Banner />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </AnimatePresence>
+      <Footer />
     </>
   );
 }
